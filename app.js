1b0f62fc082f60224a4428bd188709947888deb1
Khashayar Shomali <khashi792017@gmail.com>
2023-02-01 21:27:33 +0330

    Add Box Shadow on hover to cards & something else.

--- app.js
+++ app.js
@@ -5,5 +5,23 @@ fetch(url)
     return response.json();
   })
   .then((jsonData) => {
-    console.log(jsonData);
+    console.log("Khashayar Shomali");
+
+    let data = "";
+    jsonData.map((values) => {
+      data += `
+        <div class="card">
+        <h2 class="title">${values.title}</h2>
+        <img  class ="images" src="${values.image}" alt="" title="${values.title}" />
+        <p class="description">${values.description}</p>
+        <p class="category">${values.category}</p>
+        <p class="price">${"$" + values.price}</p>
+      </div>
+      `;
+
+      document.querySelector(".container").innerHTML = data;
+    });
+  })
+  .catch((err) => {
+    console.log(err);
   });
