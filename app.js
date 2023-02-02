53c4450a2e612d1cc00513c11564977d03620bf9
Khashayar Shomali <khashi792017@gmail.com>
2023-02-02 16:07:08 +0330

    Add loader to fetch

--- app.js
+++ app.js
@@ -1,27 +1,35 @@
-const url = "https://fakestoreapi.com/products";
+// Fetch API
+const spinner = document.querySelector(".spinner");
 
-fetch(url)
-  .then((response) => {
-    return response.json();
-  })
-  .then((jsonData) => {
-    console.log("Khashayar Shomali");
+function loadData() {
+  spinner.removeAttribute("hidden");
+  const url = "https://fakestoreapi.com/products";
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((jsonData) => {
+      spinner.setAttribute("hidden", "");
+      console.log("Khashayar Shomali");
 
-    let data = "";
-    jsonData.map((values) => {
-      data += `
+      let dataObject = "";
+      jsonData.map((values) => {
+        dataObject += `
         <div class="card">
         <h2 class="title">${values.title}</h2>
-        <img  class ="images" src="${values.image}" alt="" title="${values.title}" />
+        <img  class ="images" src="${values.image}" alt="" title="${
+          values.title
+        }" />
         <p class="description">${values.description}</p>
         <p class="category">${values.category}</p>
         <p class="price">${"$" + values.price}</p>
       </div>
       `;
 
-      document.querySelector(".container").innerHTML = data;
+        document.querySelector(".container").innerHTML = dataObject;
+      });
+    })
+    .catch((err) => {
+      console.log(err);
     });
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+}
